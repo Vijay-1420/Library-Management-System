7de9aba3e87af3dd009e669e78fdc7033202684d
Vijay-1420 <vijaysarathy1420@gmail.com>
2026-05-21 19:24:36 +0530

    Delete library management/library-pal-django-main/django_library_project directory

--- library management/library-pal-django-main/django_library_project/library_system/wsgi.py
+++
@@ -1,9 +0,0 @@
-"""
-WSGI config for library_system project.
-"""
-
-import os
-from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'library_system.settings')
-application = get_wsgi_application()
